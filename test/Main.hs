ce3944ea0ed3fdb5ca24dbb16890d14ff635c0d0
Alex Rozenshteyn <rpglover64@gmail.com>
2015-02-13 11:06:31 -0500

    Add eliminateRaw function to test file.
    The function uses the parser to substitute TeXRaw occurences
    for their parse results.

--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,19 @@
 
 import Text.LaTeX
 import Text.LaTeX.Base.Parser
+import Text.LaTeX.Base.Syntax (LaTeX(..), texmapM)
 
 import Test.Tasty
 import qualified Test.Tasty.QuickCheck as QC
 
+
 instance Eq ParseError where
   _ == _ = undefined
+
+eliminateRaw :: LaTeX -> Either ParseError LaTeX
+eliminateRaw = texmapM isRaw (parseLaTeX . render)
+  where isRaw (TeXRaw _) = True
+        isRaw _          = False
 
 main :: IO ()
 main = defaultMain $ testGroup "HaTeX"
