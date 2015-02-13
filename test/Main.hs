92aa12210cc051494ddea752a8ae81ad4a5d12f1
Alex Rozenshteyn <rpglover64@gmail.com>
2015-02-13 11:37:40 -0500

    Add test that eliminateRaw doesn't disturb parsing.

--- test/Main.hs
+++ test/Main.hs
@@ -6,6 +6,9 @@ import Text.LaTeX.Base.Syntax (LaTeX(..), texmapM)
 import Test.Tasty
 import qualified Test.Tasty.QuickCheck as QC
 
+fromRight :: Either a b -> b
+fromRight (Right x) = x
+fromRight _         = undefined
 
 instance Eq ParseError where
   _ == _ = undefined
@@ -27,6 +30,9 @@ main = defaultMain $ testGroup "HaTeX"
   , testGroup "Parser"
     [ QC.testProperty "render . parse = id" $
          \l -> let t = render (l :: LaTeX)
+               in  fmap render (parseLaTeX t) == Right t
+    , QC.testProperty "render . parse = id (eliminating RaWTeX)" $
+         \l -> let t = render . fromRight . eliminateRaw $ (l :: LaTeX)
                in  fmap render (parseLaTeX t) == Right t
     ]
   ]
